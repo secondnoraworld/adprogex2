48a6b0c6e2b2bffc1d7532d9fb9948b9e7579f53
ak1t0 <aktoo3097@gmail.com>
2017-01-17 15:35:58 +0900

    :sparkles: Remove unnecessary code

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -5,21 +5,6 @@ class GithubController < ApplicationController
   CLIENT_SECRET = ENV['GITHUB_CLIENT_SECRET']
 
   def index
-    # Octokit
-    token = @current_user.token
-    oclient = Octokit::Client.new(:access_token => token)
-
-    @data = []
-    repos = oclient.repositories(oclient.user.login)
-    repos.each do |repo|
-      h = {}
-      h.store("name", repo.name)
-      h.store("lang", repo.language)
-      h.store("url", repo.url)
-      h.store("description", repo.description)
-      @data.push(h)
-    end
-
   end
 
   def autofolio
